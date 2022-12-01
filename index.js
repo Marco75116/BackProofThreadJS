ed95f00ea039e5e07aa4fa300c6f8b308d7d04e9
marc-antoine ourradour <marc-antoineourradour@MacBook-Air-de-marc-antoine.local>
2022-12-01 17:33:05 +0100

    fix deploy

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const product = require("./api/product");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 app.use("/api/product", product);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+module.exports = app;
